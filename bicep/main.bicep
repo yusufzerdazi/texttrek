380acf4d3883176eb6d47bb9263efa4da5dc5e93
Yusuf Zerdazi <yusuf@zerdazi.com>
2023-09-11 20:47:15 +0100

    adding infra

---
+++ bicep/main.bicep
@@ -0,0 +1,20 @@
+resource storageAccount 'Microsoft.Storage/storageAccounts@2021-02-01' = {
+  name: 'texttrek'
+  location: resourceGroup().location
+  kind: 'StorageV2'
+  sku: {
+    name: 'Standard_LRS'
+  }
+  properties: {
+
+  }
+}
+
+module setStatic './setStorageStaticWebsite.ps1.bicep' = {
+  name: 'setStatic'
+  params: {
+    storageAccountName: storageAccount.name
+    staticWebsiteState: 'Enabled'
+    userAssignedIdentityName: 'AEU1-PE-CTL-D1-uaiEnableStaticWebsite'
+  }
+}
--- src/main.py
+++ src/main.py
@@ -4,10 +4,25 @@ import openai
 openai.organization = os.environ["OPEN_AI_ORGANIZATION"]
 openai.api_key = os.environ["OPEN_AI_KEY"]
 
+
+# Get latest prompts & votes from blob storage (or initial prompt)
+
+# Calculate most popular latest prompt using votes
+
+# Combine latest prompts with previous prompts into a single command
+
+# Generate next section of the story
+
+# Check if the story has reached a conclusion, if so end it
+
+# Generate accompanying image
+
 images = openai.Image.create(
     prompt="A cute baby sea otter",
-    n=2,
+    n=1,
     size="1024x1024"
 )
+
+# Download image and save to blob storage
 
 print(images)
