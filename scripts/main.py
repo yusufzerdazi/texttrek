7992304ec55bdf763d5ed17e7b4dc7b5027c6bfb
Yusuf Zerdazi <yusuf@zerdazi.com>
2023-09-13 13:43:30 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -32,7 +32,7 @@ else:
 # Combine latest prompts with previous prompts into a single command
 
 # Generate next section of the story
-text_input = "Check my inventory"
+text_input = "Decipher the runes on the altar"
 if next_index == "000":
   prompt = open(f"{root_path}/scripts/templates/initial.txt", "r").read()
 else:
